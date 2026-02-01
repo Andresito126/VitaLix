360a3ddfb20421111190aa17d66a02a4f7ac46fb
Andresito126 <159158192+Andresito126@users.noreply.github.com>
2026-02-01 16:41:03 -0600

    Add: Adding dependencies: viewModel, retrofit, converser and coil

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.secrets.gradle)
 }
 
 android {
@@ -58,4 +59,9 @@ dependencies {
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
+
+    implementation(libs.androidx.lifecycle.viewmodel.compose)
+    implementation(libs.com.squareup.retrofit2.retrofit)
+    implementation(libs.com.squareup.retrofit2.converter.json)
+    implementation(libs.io.coil.kt.coil.compose)
 }
--- app/src/main/java/com/jujus/vitalix/features/home/presentation/HomeScreen.kt
+++ app/src/main/java/com/jujus/vitalix/features/home/presentation/HomeScreen.kt
@@ -1,5 +1,6 @@
 package com.jujus.vitalix.features.home.presentation
 
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 import com.jujus.vitalix.core.shared.components.NurseHeader
@@ -7,4 +8,5 @@ import com.jujus.vitalix.core.shared.components.NurseHeader
 @Composable
 fun HomeScreen(){
     NurseHeader()
+    Text("hola")
 }
